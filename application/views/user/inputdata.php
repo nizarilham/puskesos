46fd63757ce39303b92f482c11b912be2038826d
nizarilham <nizar9465@gmail.com>
2020-07-20 00:07:41 +0700

    pull

---
+++ application/views/user/inputdata.php
@@ -0,0 +1,28 @@
+<!-- awal -->
+
+<div class="card">
+    <div class="card-header bg-primary text-white">
+        Masukan Data Penduduk
+    </div>
+    <div class="card-body">
+        <form method="post" action="">
+            <div class="form-grup">
+                <label>NIK</label>
+                <input type="text" name="tnik" class="form-control" required>
+            </div>
+            <div class="form-grup">
+                <label>Nama Lengkap</label>
+                <input type="text" name="tnama" class="form-control" required>
+            </div>
+            <div class="form-grup">
+                <label>Alamat</label>
+                <textarea class="form-control" name="talamat"></textarea>
+            </div>
+            <div>
+                <button type="submit" class="btn btn-success" name="bsimpan">Simpan</button>
+                <button type="reset" class="btn btn-danger" name="bhapus">Hapus</button>
+
+        </form>
+    </div>
+</div>
+</div>
